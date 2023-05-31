fb3e5c2a1910fcd64b3435fb00933ec50905d0e0
Javi H. Gil <javihgil@softspring.es>
2023-05-31 15:18:19 +0200

    Support lower SF versions

--- src/SfsPermissionsBundle.php
+++ src/SfsPermissionsBundle.php
@@ -4,9 +4,9 @@ namespace Softspring\PermissionsBundle;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class SfsPermissionsBundle extends AbstractBundle
+class SfsPermissionsBundle extends Bundle
 {
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
